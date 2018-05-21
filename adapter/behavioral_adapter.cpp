4b5711153cc80a6e70b82941a133e2d14dd330d3
kcodek <career_kk@yahoo.com>
2018-05-21 19:32:14 +0530

    add adapter

---
+++ adapter/behavioral_adapter.cpp
@@ -0,0 +1,156 @@
+// Purpose.  Adapter design pattern demo
+// 
+// Discussion.  LegacyRectangle's interface is not compatible with the system that would like to reuse it. 
+// An abstract base class is created that specifies the desired interface.  
+// An "adapter" class is defined that publicly inherits the interface of the abstract class, and privately inherits the implementation of the legacy component.  
+// This adapter class "maps" or "impedance matches" the new interface to the old implementation.
+
+#include <iostream>
+#include <stdio.h>
+#include <time.h>
+using std::cout;
+using std::endl;
+
+typedef int Coordinate;
+typedef int Dimension;
+
+/////////////////////////// Desired interface ///////////////////////////
+class Rectangle {
+public:
+   virtual void draw() = 0;
+};
+
+/////////////////////////// Legacy component ///////////////////////////
+class LegacyRectangle {
+public:
+   LegacyRectangle( Coordinate x1, Coordinate y1, Coordinate x2, Coordinate y2 ) 
+   {
+      x1_ = x1;  y1_ = y1;  x2_ = x2;  y2_ = y2;
+      cout << "LegacyRectangle:  create.  (" << x1_ << "," << y1_ << ") => (" << x2_ << "," << y2_ << ")" << endl; 
+   }
+   void oldDraw() 
+   {
+      cout << "LegacyRectangle:  oldDraw.  (" << x1_ << "," << y1_ << ") => (" << x2_ << "," << y2_ << ")" << endl; 
+   }
+private:
+   Coordinate x1_;
+   Coordinate y1_;
+   Coordinate x2_;
+   Coordinate y2_;
+};
+
+/////////////////////////// Adapter wrapper ///////////////////////////
+// An "adapter" class is defined that publicly inherits the interface of the abstract class, and privately inherits the implementation of the legacy component.  
+class RectangleAdapter : public Rectangle, private LegacyRectangle {      
+public:
+   RectangleAdapter( Coordinate x, Coordinate y, Dimension w, Dimension h ) : LegacyRectangle( x, y, x+w, y+h ) 
+   {
+      cout << "RectangleAdapter: create.  (" << x << "," << y << "), width = " << w << ", height = " << h << endl; 
+   }
+   virtual void draw() {
+      cout << "RectangleAdapter: draw." << endl;
+      oldDraw(); 
+      }
+};
+
+int main_legacy() 
+// int main()
+{
+   Rectangle*  r = new RectangleAdapter( 120, 200, 60, 40 );
+   r->draw();
+   return 0;
+}
+// output: 
+// LegacyRectangle:  create.  (120,200) => (180,240)
+// RectangleAdapter: create.  (120,200), width = 60, height = 40
+// RectangleAdapter: draw.
+// LegacyRectangle:  oldDraw.  (120,200) => (180,240)
+
+
+
+// Purpose.  Adapter design pattern (External Polymorphism demo)
+// 1. Specify the new desired interface
+// 2. Design a "wrapper" class that can "impedance match" the old to the new
+// 3. The client uses (is coupled to) the new interface
+// 4. The adapter/wrapper "maps" to the legacy implementation
+
+// 1. Specify the new i/f
+class ExecuteInterface { public:                  
+   virtual ~ExecuteInterface() { }
+   virtual void execute() = 0;
+};
+
+// 2. Design a "wrapper" or "adapter" class
+template <class TYPE>                             
+class ExecuteAdapter : public ExecuteInterface {  
+public:
+   ExecuteAdapter( TYPE* o, void (TYPE::*m)() ) 
+   { 
+      object = o; 
+      method = m; 
+    }
+   ~ExecuteAdapter() 
+   { 
+      delete object; 
+   }
+   // 4. The adapter/wrapper "maps" the new to the legacy implementation
+   void execute()            
+    /* the new */     
+   {
+      (object->*method)();
+   }
+private:                             
+   TYPE* object; // ptr-to-object attribute
+   void (TYPE::*method)();    /* the old */       // ptr-to-member-function attribute
+};                                                
+
+// The old: three totally incompatible classes // no common base class, // no hope of polymorphism
+class Fea { public:                               
+   ~Fea()        { cout << "Fea::dtor" << endl; }
+   void doThis() { cout << "Fea::doThis()" << endl; }
+};
+
+class Feye { public:
+   ~Feye()       { cout << "Feye::dtor" << endl; }
+   void doThat() { cout << "Feye::doThat()" << endl; }
+};
+
+class Pheau { public:
+   ~Pheau()          { cout << "Pheau::dtor" << endl; }
+   void doTheOther() { cout << "Pheau::doTheOther()" << endl; }
+};
+
+/* the new is returned */ 
+ExecuteInterface** initialize() 
+{
+   ExecuteInterface** array = new ExecuteInterface*[3]; /* the old is below */
+   array[0] = new ExecuteAdapter<Fea>(   new Fea(),     &Fea::doThis       );
+   array[1] = new ExecuteAdapter<Feye>(  new Feye(),    &Feye::doThat      );
+   array[2] = new ExecuteAdapter<Pheau>( new Pheau(),   &Pheau::doTheOther );
+   return array;
+}
+
+int main_external_polymorphism( void ) 
+//int main( void ) 
+{
+
+   //
+   main_legacy();
+
+   //external_polymorphism   
+   ExecuteInterface** objects = initialize();
+
+   for (int i=0; i < 3; i++) objects[i]->execute();  // 3. Client uses the new
+                                                     //    (polymporphism)
+   for (int i=0; i < 3; i++) delete objects[i];
+   delete objects;
+   return 0;
+}
+
+//output: 
+// Fea::doThis()
+// Feye::doThat()
+// Pheau::doTheOther()
+// Fea::dtor
+// Feye::dtor
+// Pheau::dtor
---
+++ adapter/behavioral_adapter_basic.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <stdio.h>
+#include <time.h>
+using std::cout;
+using std::endl;
+
+// Purpose.  Adapter
+//
+// Discussion.  The Adapter pattern discusses how to "wrap" the old interface of a legacy component, so that it can continue to contribute in a new system.  
+// It is about "impedance matching" an old dog with new tricks (to mix metaphors).  
+
+class ManlyTime {
+  public:
+        char* getTime() {
+            static char buf[30];
+            time_t  lt;
+            tm*     ltStruct;
+            time( &lt );
+            ltStruct = localtime(&lt);
+            strftime( buf, 30, "%H%M", ltStruct );
+            return buf;
+        }
+    };
+
+// On the right, private derivation is used to accomplish the same result.                                
+class WimpyTime : private ManlyTime 
+{   
+    public:      
+        char* getTime() 
+        {         
+            static char buf[30];
+            char *ptr, mi[3], am[3];
+            int  hr;
+            ptr = ManlyTime::getTime();
+            cout << "old interface time is " << ptr << endl;
+            strcpy( mi, &(ptr[2]) );
+            ptr[2] = '\0';
+            sscanf( ptr, "%d", &hr );
+            strcpy( am, "AM" );
+            if (hr > 12) 
+            {
+                hr -= 12;
+                strcpy( am, "PM" ); 
+            }
+            sprintf( buf, "%d:%s %s",hr, mi, am );
+            return buf;
+        }
+    };
+
+// On the left, WimpyTime "hasa" instance of the legacy component, and delegates the "heavy lifting" to it. 
+
+class WimpyTime_2 {                             
+    public:                                       
+        char* getTime() 
+        {                          
+            static char buf[30];                      
+            char *ptr, mi[3], am[3];                
+            int  hr;                                
+            ptr = imp_.getTime();                   
+            cout << "old interface time is "        
+                << ptr << endl;                      
+            strcpy( mi, &(ptr[2]) );                   
+            ptr[2] = '\0';                             
+            sscanf( ptr, "%d", &hr );               
+            strcpy( am, "AM" );                        
+            if (hr > 12) {                          
+                hr -= 12;                        
+                strcpy( am, "PM" ); }          
+            sprintf( buf, "%d:%s %s",
+                hr, mi, am );                 
+            return buf;                       
+        }                                       
+    private:                                   
+        ManlyTime  imp_;                       
+};          
+
+
+// int main_adapter( void )     
+int main()
+{
+   WimpyTime  newT;                     
+   char*      ptr;                      
+   ptr = newT.getTime();
+   cout << "new interface(private derivation) time is "<< ptr << endl;
+
+   WimpyTime_2 newT2;
+   char*      ptr2;                      
+   ptr2= newT2.getTime();
+   cout << "new interface('hasa' instance) time is "<< ptr << endl;
+
+
+   return 0;
+}
+
